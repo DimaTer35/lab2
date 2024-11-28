2307f82eae6e0c224ebd9a1264fa08ee7a74fff4
Terentev Dmitriy <terentevda@student.bmstu.ru>
2024-11-28 23:22:42 +0300

    добавил проверку в main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def add(a, b):
+    """Возвращает сумму двух чисел a и b. Проверяет, что входные данные являются числами."""
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise ValueError("Оба аргумента должны быть числами.")
     return a + b
 
 if __name__ == "__main__":
